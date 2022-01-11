207d56b8aaf583231c9d79034c83d90d47f7a677
Rommel Cysne <bcysne@yahoo.com.br>
2022-01-11 15:32:03 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,25 @@
+import "./styles.css"
+
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/rbcysne">
+
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/rbcysne</p>
+                        </div>
+                    </a>
+                </div>
+
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
